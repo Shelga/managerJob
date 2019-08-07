408e8163cb1055772fa4d7feebe05ce64b6fc6db
Olga Melnikova <shelga.melnikova@gmail.com>
2019-08-07 20:26:35 +0300

    wip

---
+++ src/Select.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Select() {
+    return (
+        <select className="select" >
+            
+        </select>
+
+         
+      
+    );
+}
+
+
+
+export default Select;
