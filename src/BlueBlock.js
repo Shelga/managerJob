d01bdcb80319d58d9605197fac9124d30d27558e
Olga Melnikova <shelga.melnikova@gmail.com>
2019-08-10 23:33:00 +0300

    wip

--- src/BlueBlock.js
+++ src/BlueBlock.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './BlueBlock.css';
 import circularParts from './static/circularParts.svg';
+import plus from './static/plus.svg';
 
 function BlueBlock() {
     return (
@@ -11,11 +12,22 @@ function BlueBlock() {
             </div>
             <div className="secondPart">
                 <h2>Choose the category</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in congue orci. see more</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in congue orci.<br />see more</p>
+                <div className="threeImg">
+                    <div className="creativeImg"></div>
+                    <div className="clercImg"></div>
+                    <div className="plusImg"></div>
+                    <div className="textUnderImg">
+                    <p>Creative</p>
+                    <p>Clerc</p>
+                    </div>
+                </div>
             </div>
             <div className="thirdPart">
                 <h2>Recommended</h2>
-                <p>Ut sollicitudin nibh ex, et mattis arcu vestibulum et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin sed suscipit enim, vel tincidunt lorem. </p>
+                <p className="borderTop">Ut sollicitudin nibh ex, et mattis arcu vestibulum et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin sed suscipit enim, vel tincidunt lorem. </p>
+                <button className="seeJob">See Jobs</button>
+
             </div>
         </div>
     );
