9e0baefefda273936534baf6f95663200c081bee
Olga Melnikova <shelga.melnikova@gmail.com>
2019-08-12 23:24:57 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js'
-import BlueBlock from './BlueBlock.js'
+import Main from './Main.js'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <BlueBlock></BlueBlock>
+      <Main></Main>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Main.css';
+import circularParts from './static/circularParts.svg';
+import plus from './static/plus.svg';
+
+function Main() {
+    return (
+        <div className="main">
+            <div className="blueBlock">
+                <div className="firstPart">
+                    <img className="circularParts" src={circularParts}></img>
+                    <h1>Manager for Job</h1>
+                </div>
+                <div className="secondPart">
+                    <h2>Choose the category</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in congue orci.<br /><a className="href" href="#">see more</a></p>
+
+
+                    <div className="imgAndText">
+                        <div className="threeImg">
+                            <div className="creativeImg">
+                                <p className="creativeText">Creative</p>
+                            </div>
+                            <div className="clercImg">
+                                <p className="clercText">Clerk</p>
+                            </div>
+                            <div className="plusImg"></div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="thirdPart">
+                    <h2>Recommended</h2>
+                    <p className="borderTop">Ut sollicitudin nibh ex, et mattis arcu vestibulum et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin sed suscipit enim, vel tincidunt lorem. </p>
+                    <button className="seeJob">See Jobs</button>
+
+                </div>
+            </div>
+
+            <div className="moreVacanciesBlock">
+                <p>See more vacancies</p>
+
+
+            </div>
+        </div>
+    );
+}
+
+export default Main;
